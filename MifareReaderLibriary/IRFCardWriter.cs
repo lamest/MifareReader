f85c7c1d153472b0f433bfa833ced44ba4e58c19
Ivan Suhih <i.suhih@di-group.biz>
2017-09-12 15:47:59 +0700

    fix internal interface

--- MifareReaderLibriary/IRFCardWriter.cs
+++ MifareReaderLibriary/IRFCardWriter.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace MifareReaderLibriary
 {
-    internal interface IMifareCardWriter
+    public interface IMifareCardWriter
     {
         Task<bool> WriteDataAsync(string readerName, ICollection<MifareKey> keys, byte[] data, CancellationToken ct);
         Task<bool> ChangeTrailersAsync(string readerName, IDictionary<MifareKey, SectorTrailer> data, CancellationToken ct);
